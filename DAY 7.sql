e6b0341eccce74e2ebbac7c4a16a91b6febab5d1
JabEfendi <129654082+JabEfendi@users.noreply.github.com>
2023-09-14 12:17:56 +0700

    Add files via upload

---
+++ DAY 7.sql
@@ -0,0 +1,10 @@
+-- CASE 3
+select distinct(a.act_fname, d.dir_fname) fname_act_dan_director
+	from actor a		
+	inner join 	movie_cast mc	
+	on mc.act_id = a.act_id
+	inner join movie_direction md
+	on md.mov_id = mc.mov_id
+	inner join director d
+	on d.dir_id = md.dir_id
+	order by fname_act_dan_director asc;
