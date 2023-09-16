ea7b6405c675f9c145a2a01d65c7d41b6c170ffd
arsyahendratno <103812197+arsyahendratno@users.noreply.github.com>
2023-09-16 21:42:39 +0700

    Update DAY 7.sql case 2

--- DAY 7.sql
+++ DAY 7.sql
@@ -3,6 +3,13 @@ SELECT mov_title
 FROM movie
 WHERE mov_title LIKE 'The%'
 
+-- CASE 2
+SELECT CONCAT (director.dir_fname, ' ', director.dir_lname) AS director_name, movie.mov_title
+FROM movie
+JOIN movie_direction ON movie.mov_id = movie_direction.mov_id
+JOIN director ON movie_direction.dir_id = director.dir_id
+WHERE director.dir_fname = 'James' AND director.dir_lname = 'Cameron';
+
 -- CASE 3
 select distinct(a.act_fname, d.dir_fname) fname_act_dan_director
 	from actor a		
